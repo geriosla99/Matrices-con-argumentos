83321bd33983d2aa302fe8f27213f5796ae5b5b7
geriosla99 <geriosla@gmail.com>
2022-08-08 19:21:57 -0500

    Agregar archivos de proyecto.

---
+++ Matrices con argumentos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Matrices_con_argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Matriz = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //invocamos el método
+            ImprimirMatriz(Matriz);
+
+            //matriz multi
+            double[,] matriz2D ={   {1, 2 },
+                                    {3,4 }  };
+            ImprimirMatrizMultidimencional(matriz2D);
+        }
+
+        static void ImprimirMatriz(int[] matrizPa)
+        {
+            int i;
+            for (i = 0; i < matrizPa.Length; i++)
+            {
+                Console.WriteLine(matrizPa[i]);
+            }
+        }
+
+        static void ImprimirMatrizMultidimencional(double[,] matriz2DPa)
+        {
+            int i;
+            int j;
+            for (i = 0; i < matriz2DPa.GetLength(0); i++)
+            {
+                Console.WriteLine("fila {0}", i);
+                for (j = 0; j < matriz2DPa.GetLength(0); j++)
+                {
+                    Console.WriteLine(matriz2DPa[i,j]);
+                }
+            }
+        }
+
+    }
+}
